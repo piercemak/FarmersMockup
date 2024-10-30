dd3c2943941df0ac0e1a37f7752c7fa9bd0fc0c8
Pierce Makombe <piercefmak@gmail.com>
2024-10-30 16:27:40 -0400

    Restoring Delete.jsx return

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -45,7 +45,47 @@ const Delete = () => {
 
     return (
         <section className={`${styles['asection']}`}>
-        Hello
+            { loading ? <p>Loading data...</p> :
+                <Box className={`${styles['acontent']}`} sx={{display:'flex', width:'70%', padding:4, flexDirection:'column', fontSize: "18px"}}>
+                    <Box>
+                        <h2>Delete {myData.name}: </h2>
+                        <Box sx={{marginTop:'20px'}}>
+                            <hr></hr>
+                        </Box>
+
+                    </Box>
+
+
+                    <Box sx={{display:'flex', width:'100%', padding:4, flexDirection:'column'}}>
+
+                        <Box sx={{display:'flex', justifyContent:'center', marginBottom:"40px", fontSize: '25px'}}>
+                            Are you sure you want to delete {myData.name}?               
+                        </Box>
+
+                        <Box sx={{display: "flex", justifyContent:"center", gap:"16px"}}>
+                            <Box 
+                                sx={{width:"30%", fontSize: '25px', display: 'flex', justifyContent: 'center', alignItems: 'center'}} 
+                                className={`${styles['dbtn']}`} 
+                                onClick={homeNav}     
+                            >
+                                Keep Project                             
+                            </Box>
+
+                            <Box 
+                                sx={{width:"30%", fontSize: '25px', display: 'flex', justifyContent: 'center', alignItems: 'center'}} 
+                                className={`${styles['cbtn']}`} 
+                                onClick={submission}       
+                            >
+                                Delete Project                             
+                            </Box>
+
+                            
+                        </Box>
+
+
+                    </Box>                    
+                </Box>
+            }
             
 
         </section>
