74c0d5e96012c1d74ccab16bc9e3609cbdce33d1
Pierce Makombe <piercefmak@gmail.com>
2024-10-30 10:48:11 -0400

    updated Axios baseURL

--- src/components/Axios.jsx
+++ src/components/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const baseURL = "http://127.0.0.1:8000/"
+const baseURL = import.meta.env.VITE_BACKEND_URL;
 const AxiosInstance = axios.create({
     baseURL: baseURL,
     timeout: 5000,
