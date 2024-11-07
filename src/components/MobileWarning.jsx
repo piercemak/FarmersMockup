d6e6437f9c45d1764473f508b99621de6ae4e925
Pierce Makombe <piercefmak@gmail.com>
2024-11-07 10:45:10 -0500

    Adjusted styling for testing 9

--- src/components/MobileWarning.jsx
+++ src/components/MobileWarning.jsx
@@ -1,21 +1,12 @@
-import { React, useEffect } from 'react'
+import React from 'react'
 import warningBackground from '../assets/images/WarningBackground.svg'
 import styles from './modules/MobileWarning.module.css' 
 
 
 const MobileWarning = () => {
-    useEffect(() => {
-        const setHeight = () => {
-          document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
-        };
-        setHeight();
-        window.addEventListener('resize', setHeight);
-        return () => window.removeEventListener('resize', setHeight);
-      }, []);
-
-
   return (
-        <div className={styles['mobile-background']} style={{ backgroundImage: `url(${warningBackground})`, height: 'calc(var(--vh, 1vh) * 100)' }}>
+    <div className={styles['wrapper']}>
+        <div className={styles['mobile-background']} style={{ backgroundImage: `url(${warningBackground})` }}>
             <div className={styles['mobile-textcontainer1']}>
                 <span className={styles['mobile-textbody1']}>Access Denied</span>
                 <div className={styles['mobile-textcontainer2']}>
@@ -26,6 +17,7 @@ const MobileWarning = () => {
                 </div>
             </div>
         </div>
+    </div>
   )
 }
 
