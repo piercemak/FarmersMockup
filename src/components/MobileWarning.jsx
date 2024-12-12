79b06499699693e7f89d26f4269a88e9d0a62382
Pierce Makombe <piercefmak@gmail.com>
2024-12-12 17:40:24 -0500

    Adjusted overflow scroll 4

--- src/components/MobileWarning.jsx
+++ src/components/MobileWarning.jsx
@@ -9,11 +9,6 @@ const MobileWarning = () => {
   return (
  
         <div className={styles['mobile-background']}>
-            <div className={styles["signature-logo"]}>
-                <img src={signature} alt="Signature" className={styles["signature-container"]}/>            
-            </div>
-
-
             <div className={styles['mobile-textcontainer1']}>
                 <span className={styles['mobile-textbody1']}>Access Denied</span>
                 <div className={styles['mobile-textcontainer2']}>
